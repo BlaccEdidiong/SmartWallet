cb6bdbcada654c954f307e6fd668943d6161c365
Edidiong <edidiongblacc@gmail.com>
2022-10-05 21:33:46 +0100

    first commit

---
+++ mySmartWallet2022/Migrations/20221005202551_initialmigration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mySmartWallet2022.Migrations
+{
+    public partial class initialmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    First_Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    MiddleName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    gender = table.Column<int>(type: "int", maxLength: 100, nullable: false),
+                    MaritalStatus = table.Column<int>(type: "int", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
---
+++ mySmartWallet2022/Models/Data/Entities/SeedHelperClass.cs
@@ -0,0 +1,47 @@
+﻿namespace mySmartWallet2022.Models.Data.Entities
+{
+    public class SeedHelperClass
+    {
+        public static async Task seed(ApplicationDbContext context)
+        {
+            if (!context.Customers.Any())
+            {
+
+                context.Customers.Add(new Customer
+
+                {
+                    FirstName = "Maxwell",
+                    LastName = "Bernard",
+                    MiddleName = "Boji",
+                    gender = GenderEnum.male,
+                    MaritalStatus = MaritalStatusEnum.Married,
+                    DateOfBirth = DateTime.Now.AddYears(-20),
+                    Country = "USA",
+                    State = "New york",
+                    City = "Weschester",
+                    Active = true,
+
+                });
+                context.Customers.Add(new Customer
+
+                {
+                    FirstName = "Stephen",
+                    LastName = "Uti",
+                    MiddleName = "Amos",
+                    gender = GenderEnum.male,
+                    MaritalStatus = MaritalStatusEnum.Single,
+                    DateOfBirth = DateTime.Now.AddYears(-9),
+                    Country = "USA",
+                    State = "Los angeles",
+                    City = "California",
+                    Active = true,
+
+                }
+                    );
+                // context.SaveChangesAsync();
+                await context.SaveChangesAsync();
+            }
+        
+        }
+    }
+}
